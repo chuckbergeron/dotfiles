23bb019fbd2aa5da2688e7673f4644fa35052ab5
Charles Bergeron <hello@charlesbergeron.ca>
2012-10-19 10:57:25 -0700

    Fixed up rails to use --binstubs when bundle install'ing, so no more 'bundle exec' in front of everything. Also, messing around with getting oh-my-zsh working properly.

--- rails/config.zsh
+++ rails/config.zsh
@@ -9,16 +9,17 @@ export RUBY_HEAP_SLOTS_GROWTH_FACTOR=1
 export RUBY_GC_MALLOC_LIMIT=79000000
 
 alias rake_pure="/usr/bin/rake"
-alias rake="bundle exec rake"
-alias guard="bundle exec guard"
+#alias rake="bundle exec rake"
+#alias guard="bundle exec guard"
 
 # Short for Rails Autotest
-alias rat="bundle exec rake"
-alias ss="./script/server --debugger"
-alias sc="./script/console"
-alias migdb="bundle exec rake db:migrate"
-alias seeddb="bundle exec rake db:seed"
-alias reload_db="bundle exec rake db:migrate:reset && bundle exec rake db:seed"
+#alias rat="bundle exec rake"
+#alias ss="./script/server --debugger"
+#alias sc="./script/console"
+alias bis="bundle install --binstubs"
+alias migdb="rake db:migrate && rake db:migrate RAILS_ENV='test'"
+alias seeddb="rake db:seed"
+alias reload_db="rake db:migrate:reset && rake db:seed"
 
 # Capiche is a plug-in written for Capistrano
 alias pull_remote_db="cap capiche:db:mirror && cap capiche:db:import"
--- ssh/aliases.zsh
+++ ssh/aliases.zsh
@@ -1,1 +1,1 @@
-alias copy_public_key="cat ~/.ssh/id_dsa.pub | pbcopy"
+alias copy_public_key="cat ~/.ssh/id_rsa.pub | pbcopy"
