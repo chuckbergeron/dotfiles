ce278d70a374c7efc0035fa3fd67732d1d2009ad
Charles Bergeron <hello@charlesbergeron.ca>
2012-10-21 11:37:55 -0700

    Organized a few things a bit better Replaced /Users/chuckbergeron with $HOME constant Resuscitated a proper prompt

--- rvm/config.zsh
+++ rvm/config.zsh
@@ -1,5 +1,7 @@
 # For ruby version manager
-if [[ -s /Users/chuckbergeron/.rvm/scripts/rvm ]] ; then source /Users/chuckbergeron/.rvm/scripts/rvm ; fi
+if [[ -s $HOME/.rvm/scripts/rvm ]] ; then source $HOME/.rvm/scripts/rvm ; fi
 ##
-# Your previous /Users/chuckbergeron/.profile file was backed up as /Users/chuckbergeron/.profile.macports-saved_2010-02-27_at_15:21:33
+# Your previous $HOME/.profile file was backed up as $HOME/.profile.macports-saved_2010-02-27_at_15:21:33
 ##
+
+PATH=$PATH:$HOME/.rvm/bin # Add RVM to PATH for scripting
