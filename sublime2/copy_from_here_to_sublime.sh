c9368ba81b89edc300a5da019142a058541377b3
Charles Bergeron <chuck.bergeron@gmail.com>
2013-08-21 13:11:41 -0700

    Okay.. I think I've worked out my sublime woes

---
+++ sublime2/copy_from_here_to_sublime.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+# Since Sublime is insane and can't handle symlinks for the User
+# directory, we'll need to manually copy stuff over.
+
+sublime_dir=~/Library/Application\ Support/Sublime\ Text\ 2/Packages
+
+# rm -rf $ZSH_DOTFILES/sublime2/User
+cp -R "$ZSH_DOTFILES/sublime2/User"             "$sublime_dir/User"
+cp -R "$ZSH_DOTFILES/sublime2/iLife 05.tmTheme" "$sublime_dir/Color Scheme - Default/"
