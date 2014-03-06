b8567b31abf2d8088866a8ee508e51cf35ca4911
Charles Bergeron <chuck.bergeron@gmail.com>
2014-03-06 11:07:19 -0800

    use rebase for git insteaad of straight pull

--- git/aliases.zsh
+++ git/aliases.zsh
@@ -1,2 +1,2 @@
-alias gfetch="git fetch && git pull"
+alias gfetch="git fetch && git pull --rebase"
 alias prune_local_branches="git branch --merged master | grep -v 'master$' | xargs git branch -d"
