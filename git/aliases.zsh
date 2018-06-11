b93e7a77117ce33b988b109f627b78615fd55f6c
Charles Bergeron <chuck.bergeron@gmail.com>
2018-06-11 12:20:56 -0700

    * improve git pruning scripts

--- git/aliases.zsh
+++ git/aliases.zsh
@@ -1,3 +1,9 @@
 alias gfetch="git co master && git fetch && git pull --rebase"
 alias gpo="git push origin"
-alias prune_local_branches="git branch --merged master | grep -v 'master$' | xargs git branch -d"
+# alias prune_local_branches="git branch --merged master | grep -v 'master$' | xargs git branch -d"
+prune_branches_like() {
+    git branch --merged $1 | grep -v "\*\|$1" | xargs -n 1 git branch -d
+}
+alias prune_like_master="prune_branches_like master"
+alias prune_like_develop="prune_branches_like develop"
+alias prune_like_staging="prune_branches_like staging"
