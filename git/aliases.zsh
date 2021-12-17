35a1681fe839f6f87e61a1a1ee4a1ddf76251936
Charles Bergeron <chuck.bergeron@gmail.com>
2021-12-17 15:29:57 -0800

    * some updates for 2021

--- git/aliases.zsh
+++ git/aliases.zsh
@@ -1,3 +1,4 @@
+alias g="git"
 alias gfetch="git co master; git co main; git fetch && git pull --rebase"
 alias gpo="git push origin"
 # alias prune_local_branches="git branch --merged master | grep -v 'master$' | xargs git branch -d"
--- system/path.zsh
+++ system/path.zsh
@@ -1,4 +1,3 @@
-# For ruby gem 'Spring' binstubs
 export PATH=~/Dropbox/bin:./bin:$PATH
 
 # Binary Paths
@@ -8,21 +7,5 @@ export PATH=$PATH:/usr/X11/bin:/opt/local/bin:/opt/local/sbin:/usr/local/sbin:$H
 # OpenSSL Path
 export PATH=/usr/local/opt/openssl/bin:$PATH
 
-# MySQL Path
-export PATH=$PATH:/usr/local/mysql/bin
-
-# Custom Mac OSX SSH Client Path
-# export PATH=$PATH:$HOME/Library/OpenSSH/bin
-
-# Haskell Paths
-# export PATH=$PATH:~/.cabal/bin
-
-# Ruby Paths
-export PATH=$PATH:~/.gem/ruby/2.5.1/bin/
-
-# ImageMagick
-export PATH=$PATH:/opt/ImageMagick/bin
-
 # NPM
 export PATH=$PATH:./node_modules/.bin
-# alias npm-exec='PATH=$(npm bin):$PATH'
