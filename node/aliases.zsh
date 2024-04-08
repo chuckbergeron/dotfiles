1cd21a3408549264ab9183cd1d641bae6d453523
Charles Bergeron <chuck.bergeron@gmail.com>
2024-04-08 13:36:13 -0700

    update npm / node aliases

--- node/aliases.zsh
+++ node/aliases.zsh
@@ -1,7 +1,11 @@
-alias nrs="npm run start"
-alias nrd="npm run dapp"
-alias nrg="npm run ganache"
-alias nrl="npm run lambda"
+alias ni='npm install'
+alias nig='npm install -g'
+alias nrs='npm run start'
+alias nrd='npm run dev'
+alias nrb='npm run build'
+alias naf='npm audit fix'
+alias nu='npm update'
+alias nt='npm test'
 
 alias ys="yarn start"
 alias yd="yarn dapp"
