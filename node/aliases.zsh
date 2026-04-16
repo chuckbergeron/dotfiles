1444559e2ff52e942d4b33c35681bdfc1e3bd3df
Charles Bergeron <chuck.bergeron@gmail.com>
2026-04-16 10:00:43 -0700

    fix slow startup

--- node/aliases.zsh
+++ node/aliases.zsh
@@ -7,6 +7,13 @@ alias naf='npm audit fix'
 alias nu='npm update'
 alias nt='npm test'
 
+alias li='npm run lint'
+alias lf='npm run lint:fix'
+alias fo='npm run lint'
+alias ff='npm run format:fix'
+
 alias ys="yarn start"
 alias yd="yarn dapp"
 alias yl="yarn lambda"
+
+alias da="direnv allow"
